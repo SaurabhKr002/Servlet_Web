bcdd50938834884e37d70dc200d4fc83a1d72cec
Saurabh Kumar <129621065+SaurabhKr002@users.noreply.github.com>
2025-03-04 16:31:26 +0530

    Saurabh Add . Database Connection and Register Servlet

---
+++ Servlet_Web/src/main/java/com/bridgelabz/servlet/DatabaseConnection.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.servlet;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/registration_db";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Saurabh@sql21";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
